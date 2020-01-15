c6a156d1c7b65247d579d549e7938414e673bb88
bensheridanedwards <bensheridanedwards@gmail.com>
2020-01-15 17:18:24 +0000

    Rubocop all passing

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,10 +9,10 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by_email(params[:session][:email])
-    
-    if @user && @user.authenticate(params[:session][:password])
+
+    if @user&.authenticate(params[:session][:password])
       session[:user_id] = @user.id
-      redirect_to '/posts', notice => "Logged In!"
+      redirect_to '/posts' 
     else
       flash.now.alert = "Invalid email or password"
       redirect_to '/login'
